73be5c0617546be2be6ccb195db014509d51e876
Adam Gyarmati <adam.gyarmati@dialoguecreatives.com>
2024-07-18 13:28:02 +0200

    init repo and frontend

---
+++ airbnb_frontend/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="">
+      AirBnB
+      <h2 className="text-airbnb">Django and next rules</h2>
+    </main>
+  );
+}
