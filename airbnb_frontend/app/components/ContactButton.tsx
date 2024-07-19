b7e83624143c92ed2fa8142a4ffa7a19e661fd0a
Adam Gyarmati <adam.gyarmati@dialoguecreatives.com>
2024-07-19 11:16:17 +0200

    Landlord detail page done

---
+++ airbnb_frontend/app/components/ContactButton.tsx
@@ -0,0 +1,9 @@
+const ContactButton = () => {
+  return(
+    <div className="mt-6 py-3 px-6 bg-airbnb text-white rounded-xl hover:bg-airbnb-dark transition duration-200 cursor-pointer">
+      Contact
+    </div>
+  )
+}
+
+export default ContactButton;
