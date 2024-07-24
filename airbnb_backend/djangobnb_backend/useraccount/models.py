0609fa7e97ef9fad51b0bd563194abd1532ed00b
Adam Gyarmati <adam.gyarmati@dialoguecreatives.com>
2024-07-24 13:55:50 +0200

    delete unnecessary code from user model

--- airbnb_backend/djangobnb_backend/useraccount/models.py
+++ airbnb_backend/djangobnb_backend/useraccount/models.py
@@ -54,18 +54,3 @@ class User(AbstractBaseUser, PermissionsMixin):
             return f"{settings.WEBSITE_URL}{self.avatar.url}"
         else:
             return ""
-
-    # groups = models.ManyToManyField(
-    #     "auth.Group",
-    #     related_name="custom_user_groups",
-    #     blank=True,
-    #     help_text="The groups this user belongs to. A user will get all permissions granted to each of their groups.",
-    #     verbose_name="groups",
-    # )
-    # user_permissions = models.ManyToManyField(
-    #     "auth.Permission",
-    #     related_name="custom_user_permissions",
-    #     blank=True,
-    #     help_text="Specific permissions for this user.",
-    #     verbose_name="user permissions",
-    # )
