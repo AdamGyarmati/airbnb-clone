6c9abab4df95111a6f0fb1b460d85121c9ba6b48
Adam Gyarmati <adam.gyarmati@dialoguecreatives.com>
2024-07-25 16:36:43 +0200

    continue add property

--- airbnb_frontend/app/components/modals/AddPropertyModal.tsx
+++ airbnb_frontend/app/components/modals/AddPropertyModal.tsx
@@ -10,6 +10,13 @@ import CustomButton from "../forms/CustomButton";
 const AddPropertyModal = () => {
   const [currentStep, setCurrentStep] = useState(1);
   const [dataCategory, setDataCategory] = useState("");
+  const [dataTitle, setDataTitle] = useState("");
+  const [dataDescription, setDataDescription] = useState("");
+  const [dataPrice, setDataPrice] = useState(0);
+  const [dataBedrooms, setDataBedrooms] = useState(0);
+  const [dataBathrooms, setDataBathrooms] = useState(0);
+  const [dataGuests, setDataGuests] = useState(0);
+
   const addPropertyModal = useAddPropertyModal();
 
   const setCategory = (category: string) => {
@@ -18,7 +25,7 @@ const AddPropertyModal = () => {
 
   const content = (
     <>
-      {currentStep === 1 ? (
+      {currentStep == 1 ? (
         <>
             <h2 className="mb-6 text-2xl">Choose category</h2>
 
@@ -32,8 +39,72 @@ const AddPropertyModal = () => {
               onClick={() => setCurrentStep(2)}
             />
         </>
+      ) : currentStep == 2 ? (
+        <>
+          <h2 className="mb-6 text-2xl">Describe your place</h2>
+
+          <div className="pt-3 pb-6 space-y-4">
+            <div className="flex flex-col space-y-2">
+              <label>Title</label>
+              <input
+                type="text"
+                value={dataTitle}
+                onChange={(e => setDataTitle(e.target.value))}
+                className="w-full p-4 border border-gray-600 rounded-xl"
+              />
+            </div>
+
+            <div className="flex flex-col space-y-2">
+              <label>Description</label>
+              <input
+                type="text"
+                value={dataDescription}
+                onChange={(e => setDataDescription(e.target.value))}
+                className="w-full p-4 border border-gray-600 rounded-xl"
+              />
+            </div>
+          </div>
+
+          <CustomButton
+            label="Previous"
+            className="mb-2 bg-black hover:bg-gray-800"
+            onClick={() => setCurrentStep(1)}
+          />
+
+          <CustomButton
+            label="Next"
+            onClick={() => setCurrentStep(3)}
+          />
+        </>
+      ) : currentStep == 3 ? (
+        <>
+           <h2 className="mb-6 text-2xl">Details</h2>
+
+           <div className="pt-3 pb-6 space-y-4">
+            <div className="flex flex-col space-y-2">
+              <label>Price per night</label>
+              <input
+                type="number"
+                value={dataPrice}
+                onChange={(e => setDataPrice(parseInt(e.target.value)))}
+                className="w-full p-4 border border-gray-600 rounded-xl"
+              />
+            </div>
+          </div>
+
+          <CustomButton
+            label="Previous"
+            className="mb-2 bg-black hover:bg-gray-800"
+            onClick={() => setCurrentStep(2)}
+          />
+
+          <CustomButton
+            label="Next"
+            onClick={() => setCurrentStep(4)}
+          />
+        </>
       ) : (
-        <p>step 2</p>
+        <p>Step 4</p>
       )}
 
     </>
